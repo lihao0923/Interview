939dd7c9fc251ba07a3ad8fd49f219a3ee6fa8d7
Peter Li <peterli@PeterdeMac-mini.local>
2022-08-07 01:48:21 +0800

    add new files

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    // 入口
+    entry: './src/index.js',
+
+    // 出口
+    output: {
+        // 虚拟打包路径，文件夹不会真正生成，而是在8080端口虚拟生成
+        path: path.join(__dirname, 'dist'),
+
+        //  打包出来的文件名称，不会真正的物理生成
+       filename: 'bundle.js'
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './public/index.html'
+        })
+    ],
+
+    devServer: {
+        // 端口号
+        port: 8081,
+        compress: false,
+        open: false
+    }
+};
